2ef7841817ab38888a8150df74b215e0cecc4ccb
KURA <126412412+K-u-r-a@users.noreply.github.com>
2025-01-20 23:18:30 +0300

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,14 @@
+cities=("Nairobi","Cape Town")
+
+print(cities)
+
+
+African_countries=("Comoros",)
+print(African_countries)
+
+
+#looping through all values in a tuple.
+West_African_Countries=("Nigeria","Ghana","Guinea")
+for country in West_African_Countries:
+ statement=f"{country} and Niger are West African countries"
+print(statement)
